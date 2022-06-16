5c147d65982b862db012aa13eeea798c0bde66a2
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-15 23:12:35 -0400

    still having basic idea but im close to out of time

--- js.js
+++ js.js
@@ -1,37 +1,54 @@
-let myBtn =document.querySelector('#start-btn');
-let initialTxt = document.querySelector('.welcome')
-const  startValueString = 'Start Quiz'
-const answer1 = document.querySelector('.answer1')
-const answer2 = document.querySelector('.answer2')
-const answer3 = document.querySelector('.answer3')
-const answer4 = document.querySelector('.answer4')
-let question = document.querySelector('.question-header')
+// getting all my query selectors and setting them into variables
 
+let welcomeTxt = document.querySelector(".welcome")
+let startBtn = document.querySelector("#start-btn")
+let viewHighScoreBtn = document.querySelector('#high-scores')
+let timer = document.querySelector('#timeLeft')
+let question1 = document.querySelector('.choice1')
+let question2 = document.querySelector('.choice2')
+let question3 = document.querySelector('.choice3')
+let question4 = document.querySelector('.choice4')
+//let questionHeader = document.querySelector(".question-header")
+
+/*
+console.log(question1)
+console.log(question2)
+console.log(question3)
+console.log(question4)
+console.log(timer) 
+console.log(viewHighScoreBtn)
+console.log(startBtn)
+console.log(welcomeTxt) 
+*/
 var questions = [
     {
       title: "Which one is a looping structure in JavaScript?",
       choices: ["All the below", "For", "While", "do-while loops"],
       answer: "All the below"
+    },{
+        title:"What is a String",
+        choices:["A text","A Number","A yarn"],
+        answer: "A text"
     }]
 
-myBtn.addEventListener('click',function(){
+   /// console.log(questions[1].title)
 
-let startValue = document.querySelector("#start-btn").textContent
-console.log(startValue)
+let startQuizTxt = 'Start Quiz'
 
-if(startValue === startValueString){
-   initialTxt.textContent ='Choose from the following answers'
-myBtn.remove() 
-question.textContent ='WHich one is a looping structure in JavaScript ?'
-answer1.textContent = '1: All the below'
-answer2.textContent = '2: For'
+let firstQuestion = questions[1].title
+//console.log(firstQuestion)
+
+
+let startAction = function(){
+    startBtn.addEventListener('click',function(){
+      if(startBtn.textContent == startQuizTxt){
+         welcomeTxt.textContent=='Choose from the following options!'
+         startBtn.remove()
+      }
+      
+    
+    })
+  
 }
 
-
-
-
-
-
-
-
-}) 
+startAction();
